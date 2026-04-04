44f76a29e7e0414c8835e17ddd37db85e2f580dd
geniuspig2986 <geniuspig2986@gmail.com>
2026-04-04 00:26:16 -0700

    building out functions and constructors

--- a5.cpp
+++ a5.cpp
@@ -1,8 +1,77 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+
 #include "a5.h"
 
 using namespace std;
 
+
 int main(){
 
+    
     return 0;
 }
+
+// ==========================================
+// PLAYER CLASS FUNCTION DEFINITIONS
+// ==========================================
+
+Player::Player(){};
+Player::~Player(){};
+bool Player::getHasAnvil() const{
+    return hasAnvil;
+} const;
+std::string Player::getPlayerSymbol(bool isAnvil, int line) const{
+    if(isAnvil && line == 1){
+        return (playerSymbol + "A" + playerSymbol);
+    } else{
+        return (playerSymbol + playerSymbol + playerSymbol)
+    }
+} ;
+std::string Player::getPlayerName() const{};
+void Player::useAnvil() {};
+
+// ==========================================
+// GAMESTATE CLASS FUNCTION DEFINITIONS
+// ==========================================
+
+bool GameState::isValidMove() const{};
+
+GameState::GameState() : board(ROWS, std::vector<Piece>(COLS, Piece::Empty)) {
+
+};
+GameState::GameState(string player1Symbol, string player2Symbol ,bool player2IsComputer) : GameState(), {
+
+}
+GameState::~GameState(){
+
+};
+void GameState::renderGame() const{
+    for(int r = 0; r < ROWS; r++){
+        for(int line = 0; line < 3; line ++){
+            for(int c = 0; c < COLS; c++){
+                if(board[r][c] == Piece::Empty){
+                    cout << "   ";
+                } if(board[r][c] == Piece::Player1){
+                    cout << player1.getPlayerSymbol(false, line);
+                } if(board[r][c] == Piece::Player2){
+                    cout << player2.getPlayerSymbol(false, line);
+                } if(board[r][c] == Piece::Player1Anvil){
+                    cout << player1.getPlayerSymbol(true, line);
+                } if(board[r][c] == Piece::Player1Anvil){
+                    cout << player1.getPlayerSymbol(true, line);
+                } 
+            }
+            cout << " ";
+        }
+        cout << endl;
+    }
+    cout << "--------------------------------" << endl;
+};
+bool GameState::makeMove(int col, Piece piece){ //boolean so that isValidMove() can return to main
+
+}; 
+bool GameState::checkWin() const{};
+bool GameState::checkTie() const{};
--- a5.h
+++ a5.h
@@ -1,14 +1,28 @@
+#pragma once
 #include <string>
 #include <vector>
 #include <iostream>
 
+enum class Piece{
+    Empty,
+    Player1,
+    Player2,
+    Player1Anvil,
+    Player2Anvil,
+};
+
 class Player{
     private:
+        std::string playerName;
+        std::string playerSymbol; // Will still enforce one letter, but string makes concatenation easier down the line
         bool hasAnvil;
+        bool isComputer;
     public:
         Player(){};
         ~Player(){};
         bool getHasAnvil() const;
+        std::string getPlayerSymbol(bool isAnvil, int line) const;
+        std::string getPlayerName() const;
         void useAnvil();
 
 };
@@ -16,9 +30,20 @@ class Player{
 class GameState{
     private:
         bool isPVP;
-        std::vector<std::vector<char>> gameState;
+        Player player1;
+        Player player2;
+        
+        const int ROWS = 6;
+        const int COLS = 7;
+        std::vector<std::vector<Piece>> board;
+
+        bool isValidMove() const;
     public:
         GameState();
+        GameState(bool player2IsComputer);
         ~GameState();
         void renderGame() const;
+        bool makeMove(int col, Piece piece); //boolean so that isValidMove() can return to main
+        bool checkWin() const;
+        bool checkTie() const;
 };
